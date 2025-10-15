90ea432e261e6fc0d8ff0702880285923c93e7c8
AzizbekX7 <azizbekroxataliyev27@gmail.com>
2025-10-15 18:07:04 +0500

    addu qo'shildi

---
+++ src/Abduali.java
@@ -0,0 +1,30 @@
+public class Abduali {
+
+    String name;
+    String surname;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
